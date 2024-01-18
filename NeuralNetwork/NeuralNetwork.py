425022f3eff9923fb8f4c4294f1b844451c2082d
Lucie DN <ldellanegra@ensc.fr>
2024-01-18 16:14:05 +0100

    Prise en main

---
+++ NeuralNetwork/NeuralNetwork.py
@@ -0,0 +1,19 @@
+# A faire avant :
+# pip install numpy as np
+
+import numpy as np
+
+#        N1 N2 N3
+inputs = [[1,2,3],#input1
+         [2,2,3],#input2
+         [4,5,2]]#input3
+
+#         N1 N2 N3
+weights = [[1,1,1],# output node 1
+           [2,2,2]]# output node 2
+
+#       N1 N2 N3
+biases = [1,2]
+
+output = np.dot(inputs, np.array(weights).T)+biases
+print(output)
