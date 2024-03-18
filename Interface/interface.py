301735d457ad301e378609504e69c6704ba0c8aa
Lucie DN <ldellanegra@ensc.fr>
2024-03-18 18:30:33 +0100

    Mise en forme de l'interface

--- Interface/interface.py
+++ Interface/interface.py
@@ -18,7 +18,12 @@ row_frame.columnconfigure(0, weight=1)
 row_frame.columnconfigure(1, weight=1)
 row_frame.grid(row=0, column=0, sticky="nsew")
 
-top_frame = Frame(row_frame, width=1000, height=100, bg='blue')
+top_frame = Frame(row_frame, width=1000, height=100)
+top_frame.rowconfigure(0, weight=1)
+top_frame.rowconfigure(1, weight=1)
+top_frame.columnconfigure(0, weight=1)
+top_frame.columnconfigure(1, weight=1)
+top_frame.columnconfigure(2, weight=1)
 top_frame.grid(row=0, column=0, padx=10, pady=5, sticky="nsew")
 top_frame.grid_propagate(0)
 
@@ -27,13 +32,26 @@ column_frame.columnconfigure(0, weight=1)
 column_frame.columnconfigure(1, weight=1)
 column_frame.grid(row=1, column=0)
 
+# Format top frame
+title = tkinter.Label(top_frame, text="Dictionnaire de la langue des signes", font=("Helvetica", 14, 'bold', ))
+title.grid(row=0, column=0,padx=5,pady=5, sticky="wn")
+
+label1 = tkinter.Label(top_frame, text="Français", font=("Helvetica", 12, 'bold'))
+label1.grid(row=1, column=0,padx=5,pady=5, sticky="e")
+
+button = tkinter.Button(top_frame, width=10, text = "<---------->", command = lambda: updateEntry(entry, closer, list))
+button.grid(row = 1,column = 1,padx=5,pady=5)
+
+label2 = tkinter.Label(top_frame, text="Langue des signes belge francophone", font=("Helvetica", 12, 'bold'))
+label2.grid(row=1, column=2,padx=5,pady=5, sticky="w")
+
 # Create left and right frames
-left_frame = Frame(column_frame, width=300, height=500, bg='red')
+left_frame = Frame(column_frame, width=300, height=500, highlightbackground="grey", highlightthickness=3)
 left_frame.grid(row=1, column=0, padx=10, pady=5, sticky="nsew")
 left_frame.grid_propagate(0)
 left_frame.pack_propagate(0)
 
-right_frame = Frame(column_frame, width=700, height=500, bg='green')
+right_frame = Frame(column_frame, width=700, height=500)
 right_frame.grid(row=1, column=1, padx=10, pady=5, sticky="nsew")
 right_frame.rowconfigure(1, weight=1)
 right_frame.rowconfigure(1, weight=1)
@@ -54,7 +72,7 @@ searching_frame.pack_propagate(0)
 # renvoie la valeur de la liste la plus proche
 def updateEntry(entry, sv_closer, list):
     value = entry.get()
-    sv_closer.set("")
+    sv_closer.set("Pas de mot correspondant")
     matches = get_close_matches(value.upper(), list)
     if matches:
         sv_closer.set(matches[0])
@@ -62,7 +80,9 @@ def updateEntry(entry, sv_closer, list):
 
 sv = tkinter.StringVar()
 closer= tkinter.StringVar()
-list  =['OUI', 'AUSSI', 'LS']
+closer.set("Pas de mot correspondant")
+
+list = ['OUI', 'AUSSI', 'LS']
 
 entry = tkinter.Entry(searching_frame, textvariable=sv, validate="focusout")
 entry.grid(row = 0,column = 0,padx=5,pady=5, sticky="nsew")
@@ -70,16 +90,19 @@ button = tkinter.Button(searching_frame, width=50, text = "Rechercher", command 
 button.grid(row = 0,column = 1,padx=5,pady=5, sticky="nsew")
 
 # Afficher le mot le plus proche
-word = tkinter.Label(left_frame, background="pink", textvariable=closer)
-word.grid(row=1, column=0,padx=5,pady=5, sticky="wn")
+label3 = tkinter.Label(left_frame, text="Mot le plus proche de la base de données :")
+label3.grid(row=1,column=0,padx=5,pady=5, sticky="nw")
+
+word = tkinter.Label(left_frame, textvariable=closer, font=("Helvetica", 8, 'bold'))
+word.grid(row=2, column=0,padx=5,pady=5, sticky="wn")
 
 
 # Structure right side
-parameters_frame = Frame(right_frame, width=700, height=50, bg='brown')
+parameters_frame = Frame(right_frame, width=700, height=50, bg='grey')
 parameters_frame.grid(row=0, column=0)
 parameters_frame.grid_propagate(0)
 parameters_frame.pack_propagate(0)
-list_frame =  Frame(right_frame, width=700, height=450, bg='yellow')
+list_frame =  Frame(right_frame, width=700, height=450,highlightbackground="grey", highlightthickness=3)
 list_frame.grid(row=1, column=0)
 list_frame.grid_propagate(0)
 list_frame.pack_propagate(0)
@@ -123,7 +146,7 @@ videoplayer.seek(1)
 # videoplayer.grid(sticky="nw")
 
 button = tkinter.Button(parameters_frame, width=10, text = "Rafraichir", command = lambda: updateLink(closer, sv_link) )
-button.grid(padx=5,pady=5, sticky="nsew")
+button.grid(row=0, column=0, padx=5, pady=5, sticky="se")
 
 # video_link = tkinter.Label(list_frame, background="pink", textvariable=sv_link)
 # video_link.grid(row=1, column=0,padx=5,pady=5, sticky="wn")
